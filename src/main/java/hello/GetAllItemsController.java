96b8478a9a66619aa6ce59543b7ce95fab5bdfb2
Andy Evans <andrew.evans@metoffice.gov.uk>
2018-02-14 10:47:28 +0000

    can scan and retreive all items from dynamo

--- src/main/java/hello/GetAllItemsController.java
+++ src/main/java/hello/GetAllItemsController.java
@@ -1,7 +1,50 @@
 package hello;
 
-/**
- * Created by andrew.evans on 14/02/2018.
- */
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.ItemCollection;
+import com.amazonaws.services.dynamodbv2.document.ScanOutcome;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.spec.ScanSpec;
+import com.amazonaws.services.dynamodbv2.document.utils.NameMap;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Iterator;
+
+@RestController
 public class GetAllItemsController {
+    @RequestMapping("/getitems")
+    public String getItemsFromDynamoDB() {
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:8002", "us-west-2"))
+                .build();
+
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+        Table table = dynamoDB.getTable("Beaches");
+
+        ScanSpec scanSpec = new ScanSpec();
+
+        try {
+            ItemCollection<ScanOutcome> items = table.scan(scanSpec);
+
+            Iterator<Item> iter = items.iterator();
+            while (iter.hasNext()) {
+                Item item = iter.next();
+                System.out.println(item.asMap().get("beach"));
+            }
+
+        }
+        catch (Exception e) {
+            System.err.println("Unable to scan the table:");
+            System.err.println(e.getMessage());
+        }
+
+        return "got all items";
+    }
 }
