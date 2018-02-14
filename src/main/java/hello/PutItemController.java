8e894de1daf858d92d237529f20baa313c029b16
Andy Evans <andrew.evans@metoffice.gov.uk>
2018-02-14 13:58:00 +0000

    added more controller work still broke

--- src/main/java/hello/PutItemController.java
+++ src/main/java/hello/PutItemController.java
@@ -4,17 +4,30 @@ import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.dynamodbv2.document.UpdateItemOutcome;
 import com.amazonaws.services.dynamodbv2.document.spec.UpdateItemSpec;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import hello.jsontemplates.Beaches;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
+@Controller
 public class PutItemController {
 
-    @RequestMapping("/putitem")
-    public String putItemIntoDB() {
+    @PostMapping(value="/putitem", produces = {"application/json"}, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Beaches putItemIntoDB(@RequestBody Beaches inputBeaches) {
+
+        System.out.println("the beaches going in are");
+        System.out.println(inputBeaches);
 
         AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
                 .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:8002", "us-west-2"))
@@ -24,21 +37,26 @@ public class PutItemController {
 
         Table table = dynamoDB.getTable("Beaches");
 
+        List<String> beaches = new ArrayList<>();
+
+        beaches.add("Exmouth");
+        beaches.add("Dawlish");
+
         String beach = "Exmouth";
 
-        UpdateItemSpec updateItemSpec = new UpdateItemSpec().withPrimaryKey("beach", beach);
-
         try {
-            System.out.println("Updating the item...");
-            UpdateItemOutcome outcome = table.updateItem(updateItemSpec);
-            System.out.println("UpdateItem succeeded:\n" + outcome.getItem().toJSONPretty());
-
+            for (String site : beaches) {
+                Item item = new Item().withPrimaryKey("beach", site);
+                System.out.println("Updating the item...");
+                table.putItem(item);
+                System.out.println("UpdateItem succeeded:\n");
+            }
         }
         catch (Exception e) {
             System.err.println("Unable to update item: " + beach);
             System.err.println(e.getMessage());
         }
 
-        return "done";
+        return new Beaches(new ArrayList<>());
     }
 }
