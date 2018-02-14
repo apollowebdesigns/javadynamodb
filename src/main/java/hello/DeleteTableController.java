4505fa00027e2cce4b7b77dc4f9f0dd6af1bf088
Andy Evans <andrew.evans@metoffice.gov.uk>
2018-02-14 10:07:19 +0000

    added new changes

--- src/main/java/hello/DeleteTableController.java
+++ src/main/java/hello/DeleteTableController.java
@@ -1,7 +1,44 @@
 package hello;
 
-/**
- * Created by andrew.evans on 14/02/2018.
- */
+
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+@RestController
 public class DeleteTableController {
+
+    public static final String DYNAMODB = "http://localhost:8002";
+
+    @RequestMapping("/delete/table")
+    public void createTable() {
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(DYNAMODB, "us-west-2"))
+                .build();
+
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+        String tablename = "Beaches";
+
+        Table table = dynamoDB.getTable(tablename);
+
+        try {
+            System.out.println("Attempting to delete table; please wait...");
+            table.delete();
+            table.waitForDelete();
+            System.out.print("Success.");
+
+        }
+        catch (Exception e) {
+            System.err.println("Unable to delete table: ");
+            System.err.println(e.getMessage());
+        }
+    }
+
 }
